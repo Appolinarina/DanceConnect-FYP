97bbec9b3b5847518e0a239c77c8bd8ef6317200
Appolinarina <polinaperevets@gmail.com>
2025-11-04 11:07:15 +0000

    connected to mongoDB database with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const classRoutes = require('./routes/classes')
 
 //express app
@@ -16,7 +17,14 @@ app.use((req, res, next) => {
 //routes
 app.use('/api/classes', classRoutes)
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+//connect to db through URI
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to database and listening on port', process.env.PORT)
+        })
+    }) //fire funciton when complete
+    .catch((error) => { 
+        console.log(error)
+    })
