29a6fec35c65e2545ee8a0297dd819083780cbb9
Appolinarina <polinaperevets@gmail.com>
2026-01-31 22:13:52 +0000

    implemented user login and token creation in user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,18 @@ const createToken = (_id) => {
 
 //login user
 const loginUser = async (req, res) => {
-    res.json({mssg: 'user login'})
+    const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password) //login function from userModel
+
+        //create json web token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token}) //send back email that the user login with, and the token
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 //signup user
