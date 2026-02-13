0bc31609c8910912d9a5d0cf8a4a7b756897f868
Appolinarina <polinaperevets@gmail.com>
2026-02-13 10:44:10 +0000

    made useSignup hook

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,12 +1,14 @@
 import {useState} from 'react'
+import { useSignup } from '../hooks/useSignUp'
 
 const Signup = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const {signup, error, isLoading} = useSignup()
 
     const handleSubmit = async(e) => {
         e.preventDefault() //prevent page refresh upon hitting submit button
-        console.log(email, password)
+        await signup(email, password) // pass in inputed email and pass from form (that are being stored in state)
     }
 
     return(
@@ -19,7 +21,8 @@ const Signup = () => {
             <label>Password:</label> 
             <input type="password" onChange={(e)=>setPassword(e.target.value)} value = {password}/> 
 
-            <button>Sign Up</button>
+            <button disabled={isLoading}>Sign Up</button>
+            {error && <div className="error">{error}</div> }
         </form>
     )
 }
